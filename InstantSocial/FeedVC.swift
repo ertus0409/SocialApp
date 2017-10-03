518ea4037748d590b2b2520c46047fa847b11cfe
ertus0409 <ertus0409@hotmail.com>
2017-10-04 01:21:43 +0300

    Firebase git complete.

--- InstantSocial/FeedVC.swift
+++ InstantSocial/FeedVC.swift
@@ -15,6 +15,9 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     //IBOUTLETS:
     @IBOutlet weak var tableView: UITableView!
+    
+    //VARIABLES:
+    var posts = [Post]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +26,18 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.dataSource = self
         
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
-            print(snapshot.value!)
+            self.posts = []
+            if let snapshots = snapshot.children.allObjects as? [DataSnapshot] {
+                for snap in snapshots {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
         })
 
         
@@ -34,10 +48,13 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func  tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print("POST: \(post.caption)")
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
 
